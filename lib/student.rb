d1632f4599887c76675e563bfb75991d0c016364
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 15:01:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,37 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade, :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS students(id INTEGER AUTO INCREMENT, name TEXT, grade INTEGER)")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    if @id
+      self.update
+    else
+      DB[:conn].execute("INSERT INTO students(name, grade) VALUES (?, ?)", @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
